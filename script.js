93073b24ba1c863666a39a442e716241ceb657d0
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-02-10 08:09:38 +0500

    extra log

---
+++ script.js
@@ -0,0 +1,14 @@
+
+     
+     document.addEventListener("DOMContentLoaded", function() {
+     
+        var introDuration = 3000; // 3 seconds
+
+       
+        var mainWebsiteURL = "https://hussainshamim16.github.io/sasta-biryani/";
+
+      
+        setTimeout(function() {
+            window.location.href = mainWebsiteURL;
+        }, introDuration);
+    });
